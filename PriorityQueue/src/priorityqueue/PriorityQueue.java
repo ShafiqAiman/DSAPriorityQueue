8a92729dd671a817cb6072e7197e67fe3492529e
Dakatsaku <41362027+Dakatsaku@users.noreply.github.com>
2019-01-07 21:53:15 +0800

    Update PriorityQueue.java

--- PriorityQueue/src/priorityqueue/PriorityQueue.java
+++ PriorityQueue/src/priorityqueue/PriorityQueue.java
@@ -1,8 +1,9 @@
+package GroupProject;
+
 /*
  * Data Structure & Algorithm Group Project 
  * This java file illustrates the priority queue code and its operation.
  */
-package priorityqueue;
 
 /**
  * Group Members: 1) Shahrain binti Johan (B04180045)
@@ -13,41 +14,66 @@ package priorityqueue;
  */
 
 import java.util.*;
-import javax.swing.JOptionPane;
 
-public class PriorityQueue {
+class DSAGroupProject {
     public static void main(String[] args) {
         
         //Creating an empty priority queue
-        priorityQueue<String> pQueue = new priorityQueue<String>();
+        PriorityQueue<String> pQueue = new PriorityQueue<String>();
         
-        //Asking user to enter item into the list
-        String amount = JOptionPane.showInputDialog(null, "Enter the amount of items you want in the queue: ", "Priority Queue", JOptionPane.QUESTION_MESSAGE);
-        int a = Integer.parseInt();
-        for(int i = 0; i <= a; i++){
-            String input = JOptionPane.showInputDialog(null, "Enter the item into priority queue: ", "Priority Queue", JOptionPane.QUESTION_MESSAGE);
-            pQueue.add(input);
-        }//end of for loop
+        //Operation 1: Adding items to the priority queue
+        pQueue.add("Orange");
+        pQueue.add("Strawberry");
+        pQueue.add("Apple");
+        pQueue.add("Grape");
+        pQueue.add("Kiwi");
         
-        //Displaying the element with the highest priority
+        //Operation 2: Displaying the element with the highest priority
         System.out.println("Head value using peek function: " + pQueue.peek());
-        
         System.out.println("");
         
-        //Printing all the elements
+        //Operation 3: Returns the size of the priority queue
+        System.out.println("Size of the priority queue: " + pQueue.size());
+        System.out.println("");
+        
+        //Operation 4: Displaying all the elements
         System.out.println("The queue elements: ");
         Iterator itr = pQueue.iterator();
         while(itr.hasNext())
             System.out.println(itr.next());
+        System.out.println("");
         
-        //Removes the element with the highest priority and printing the modified pQueue using poll()
+        //Operation 5: Removes the element with the highest priority and printing the modified pQueue using poll()
         pQueue.poll();
-        System.out.println("After removing the an element with the poll function: ");
+        System.out.println("After removing the element with highest priority using the poll function: ");
         Iterator<String> itr2 = pQueue.iterator();
         while(itr2.hasNext())
             System.out.println(itr2.next());
+        System.out.println("");
         
-        //Removes the 
-
+        //Removing Orange using remove() function
+        pQueue.remove("Orange");
+        System.out.println("After removing 'Orange' with remove() function: ");
+        Iterator<String> itr3 = pQueue.iterator();
+        while(itr3.hasNext())
+            System.out.println(itr3.next());
+        System.out.println("");
+        
+        //Operation 6: Checks if an element is present inside the queue using contains() method
+        boolean result = pQueue.contains("Strawberry");
+        System.out.println("Does the priority queue contain 'Strawberry?: " + result);
+        System.out.println("");
+        
+        //Operation 7: Getting objects from the queue using toArray() in an array and print the array
+        Object[] array = pQueue.toArray();
+        System.out.println("Current values in the array: ");
+        for (Object i : array) {
+            System.out.println(i.toString());
+        } //end for loop
+        System.out.println("");
+        
+        //Operation 8: Clear the contents in the priority queue
+        pQueue.clear();
+        System.out.println("Values in the priority queue after using clear() method: " + pQueue);
     }//end of main
 }//end of class PriorityQueue
